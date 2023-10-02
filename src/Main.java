9a12b924e587b749d030f910a5fb90c3a70193f7
konradv <vladyslav.konrad@auk.edu.ua>
2023-10-02 10:44:18 +0300

    Homework5

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int sum = addNumbers(5, 10);
+
+            System.out.println("The sum of 5 and 10 is: " + sum);
+        }
+
+
+        public static int addNumbers(int num1, int num2) {
+            int sum = num1 + num2;
+            return sum;
+        }
+}
